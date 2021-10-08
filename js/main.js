678ac505b7ab119fb1f68c123579ac1c86e6640c
suyeon-hong <bellahong95@gmail.com>
2021-10-08 15:33:46 +0900

    youtube data 불러오기

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $.ajax({
     url: "https://www.googleapis.com/youtube/v3/playlistItems",
-    datatype: "jsonp",
+    dataType: "jsonp",
     data: {
         part: "snippet",
         key: "AIzaSyDLlgfKVBsGJubow2HpPxjC1LJgGRtpBHA",
@@ -8,7 +8,56 @@ $.ajax({
         playlistId: "PLbO44G2j_RJzPLODoK6qiJwJ39JK4JyCY"
     }
 }).success(function(data){
-    console.log(data);
+    let items = data.items;
+    console.log(items);
+
+    $(items).each(function(index, data){
+        $("#vidGallery").append(
+            $("<article>").append(
+                $("<a>").attr({href: data.snippet.resourceId.videoId}).append(
+                    $("<img>").attr({src: data.snippet.thumbnails.high.url})
+                ),
+                $("<div class='con'>").append(
+                    $("<h2>").text(data.snippet.title),
+                    $("<p>").text(data.snippet.description),
+                    $("<span>").text(data.snippet.publishedAt)
+                )
+            )
+        )
+    });
 }).error(function(err){
     console.error(err);
 });
+
+/*
+$.ajax({
+    url: "https://www.googleapis.com/youtube/v3/playlistItems",
+    dataType: "jsonp",
+    data: {
+        part: "snippet",
+        key: "AIzaSyDLlgfKVBsGJubow2HpPxjC1LJgGRtpBHA",
+        maxResult: 3,
+        playlistId: "PLbO44G2j_RJzPLODoK6qiJwJ39JK4JyCY"
+    }
+}).success(function(data){
+    let items = data.items;
+    console.log(items);
+
+    $(items).each(function(index, data){
+        $("#vidGallery").append(
+            $("<article>").append(
+                $("<a>").attr({href: data.snippet.resourceId.videoId}).append(
+                    $("<img>").attr({src: data.snippet.thumbnails.high.url})
+                ),
+                $("<div class='con'>").append(
+                    $("<h2>").text(data.snippet.title),
+                    $("<p>").text(data.snippet.description),
+                    $("<span>").text(data.snippet.publishedAt)
+                )
+            )
+        )
+    });
+}).error(function(err){
+    console.error(err);
+});
+*/
