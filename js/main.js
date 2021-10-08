bbbb3f255f6cfe1cc7b7a9650baf3783e8b54907
suyeon-hong <bellahong95@gmail.com>
2021-10-08 15:46:38 +0900

    텍스트말줄임표붙이기, 날짜텍스트정리

--- js/main.js
+++ js/main.js
@@ -12,6 +12,18 @@ $.ajax({
     console.log(items);
 
     $(items).each(function(index, data){
+        // 본문 텍스트 내용이 200글자를 넘어가면 말줄임표 붙이기
+        let txt = data.snippet.description;
+        let len = txt.length;
+
+        if(len > 200){
+            txt = txt.substr(0, 200) + "...";
+        }
+
+        // 날짜 텍스트 정리
+        let date = data.snippet.publishedAt;
+        date = date.split("T")[0];
+
         $("#vidGallery").append(
             $("<article>").append(
                 $("<a>").attr({href: data.snippet.resourceId.videoId}).append(
@@ -19,8 +31,8 @@ $.ajax({
                 ),
                 $("<div class='con'>").append(
                     $("<h2>").text(data.snippet.title),
-                    $("<p>").text(data.snippet.description),
-                    $("<span>").text(data.snippet.publishedAt)
+                    $("<p>").text(txt),
+                    $("<span>").text(date)
                 )
             )
         )
